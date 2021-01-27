cfa702334e4d079bb40dc27c26e3ac552fc7f7fd
zky44 <77955005+zky44@users.noreply.github.com>
2021-01-27 18:02:02 -0500

    Following an exercise
    we pasted in some stuff and blah blah blah (what you did and why you did it)

---
+++ my_script.py
@@ -0,0 +1,9 @@
+# This is an example python code cell
+# Run it and see the output below
+# These lines are "comments", starting with the #
+# Don't be confused that # means something different in .md and .py files.
+
+print("HELLO WORLD!")
+
+x = 2 + 2
+print(x)
