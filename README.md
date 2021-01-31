38558652c9554dec8fa865eb5891bc1c3922cf32
zky44 <77955005+zky44@users.noreply.github.com>
2021-01-31 13:48:27 -0500

    Practice
    Made these changes when practicing on my own

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+# TryingThisAgain!
 
 # My First Repo!
 
---
+++ my_scriptTWO.py
@@ -0,0 +1,11 @@
+# Re-Doing This Exercise
+
+# This is an example python code cell
+# Run it and see the output below
+# These lines are "comments", starting with the #
+# Don't be confused that # means something different in .md and .py files.
+
+print("HELLO WORLD!")
+
+x = 2 + 4
+print(x)
